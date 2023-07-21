1fa843e2f9c5efe5b1b3794ade588047f1773906
Lin <CLin@asrcfederal.com>
2023-07-21 12:35:48 -0400

    included drawinng menu

--- app/src/main/java/com/example/metiscodeathonpi22/MapsMarkerActivity.java
+++ app/src/main/java/com/example/metiscodeathonpi22/MapsMarkerActivity.java
@@ -1,13 +1,19 @@
 package com.example.metiscodeathonpi22;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -22,6 +28,8 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 
 import java.io.IOException;
@@ -38,21 +46,23 @@ import okhttp3.Response;
 
 public class MapsMarkerActivity extends AppCompatActivity
         implements OnMapReadyCallback,
-        GoogleMap.OnMarkerClickListener
-{
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMapLongClickListener {
     private GoogleMap mMap;
+    private GoogleMap.OnMapLongClickListener mapLongClickListener;
+    private GoogleMap.OnMapClickListener mapClickListener;
     private List<Marker> markerList = new ArrayList<>();
-    private static final String POST_API_URL =
-            "https://5ajph3kb1d.execute-api.us-east-2.amazonaws.com/createGeotag";
-    private static final String GET_API_URL =
-            "https://5ajph3kb1d.execute-api.us-east-2.amazonaws.com/getGeotag";
+    private boolean isDrawingEnabled = false;
+    private Polygon activePolygon;
+    private List<LatLng> polygonPoints = new ArrayList<>();
+
+    private List<Polygon> polygons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        fetchDataFromApi();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -61,38 +71,98 @@ public class MapsMarkerActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        LatLng usdaDC = new LatLng(38.8867712,-77.0325009);
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(usdaDC, 15f);
-        googleMap.animateCamera(cameraUpdate);
+            mMap = googleMap;
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            LatLng usdaDC = new LatLng(38.8867712, -77.0325009);
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(usdaDC, 15f);
+            mMap.animateCamera(cameraUpdate);
 
-        String strLatitude = Location.convert(usdaDC.latitude, Location.FORMAT_DEGREES);
-        String strLongitude = Location.convert(usdaDC.longitude, Location.FORMAT_DEGREES);
+            String address = convertLatLngToAddress(usdaDC);
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(usdaDC)
+                    .title("US Department of Agriculture")
+                    .snippet(address);
 
-        MarkerOptions markerOptions = new MarkerOptions()
-                .position(usdaDC)
-                .title("US Department of Agriculture")
-                .snippet(strLatitude + " " + strLongitude);
+            Marker marker = googleMap.addMarker(markerOptions);
+            markerList.add(marker);
+            marker.showInfoWindow();
 
-        Marker marker = googleMap.addMarker(markerOptions);
-        markerList.add(marker);
-        marker.showInfoWindow();
-        googleMap.setOnMarkerClickListener(this);
-        googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                enterDescriptionDialog(latLng);
+            mMap.setOnMarkerClickListener(this);
+            mMap.setOnMapLongClickListener(this);
+            mMap.setOnMapClickListener(mapClickListener);
 
-            }
-        });
     }
 
+    //Drawing
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if (isDrawingEnabled) {
+            // Add the clicked location to the list of polygon points
+            polygonPoints.add(latLng);
+
+            // Update the active polygon on the map
+            if (activePolygon != null) {
+                activePolygon.remove();
+            }
+
+            // Create a PolygonOptions object with the updated points and properties
+            PolygonOptions polygonOptions = new PolygonOptions()
+                    .addAll(polygonPoints)
+                    .strokeColor(Color.RED)
+                    .fillColor(Color.BLUE)
+                    .strokeWidth(5f);
+
+            // Add the updated polygon to the map
+            activePolygon = mMap.addPolygon(polygonOptions);
+        }
+    }
+    public void onStartDrawingClick(View view) {
+        isDrawingEnabled = true;
+        polygonPoints.clear();
+        // Enable map clicks
+        mMap.setOnMapClickListener(this);
+
+        // Show "Stop Drawing" button and hide "Start Drawing" button
+        Button btnStartDrawing = findViewById(R.id.btnStartDrawing);
+        Button btnStopDrawing = findViewById(R.id.btnStopDrawing);
+        btnStartDrawing.setVisibility(View.GONE);
+        btnStopDrawing.setVisibility(View.VISIBLE);
+    }
+    public void onStopDrawingClick(View view) {
+        isDrawingEnabled = false;
+        mMap.setOnMapClickListener(null);
+
+        Polygon newPolygon = mMap.addPolygon(new PolygonOptions()
+                .addAll(polygonPoints)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE)
+                .strokeWidth(5f));
+
+        polygons.add(newPolygon);
+
+        Button btnStartDrawing = findViewById(R.id.btnStartDrawing);
+        Button btnStopDrawing = findViewById(R.id.btnStopDrawing);
+        btnStartDrawing.setVisibility(View.VISIBLE);
+        btnStopDrawing.setVisibility(View.GONE);
+    }
+
+    // enter comment
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        enterDescriptionDialog(latLng);
+    }
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        showLongToast(this,
+                marker.getTitle() + "\n" + marker.getPosition(),
+                6000 );
+        return false;
+    }
     private void enterDescriptionDialog(final LatLng latLng) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setTitle("\nEnter title for the following position");
-        builder.setMessage(latLng.toString() + ":");
+        builder.setTitle("\nEnter comment for the following position");
+        builder.setMessage(convertLatLngToAddress(latLng) + ":");
         final EditText input = new EditText(this);
         builder.setView(input);
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -115,117 +185,105 @@ public class MapsMarkerActivity extends AppCompatActivity
 
         builder.show();
     }
-
-    private void dropMarker(LatLng latLng, String titleValue) {
-        // Add a marker to the map
-        String strLatitude = Location.convert(latLng.latitude, Location.FORMAT_DEGREES);
-        String strLongtitude = Location.convert(latLng.longitude, Location.FORMAT_DEGREES);
-
-        MarkerOptions markerOptions = new MarkerOptions()
-                .position(latLng)
-                .title(titleValue)
-                .snippet(strLatitude + " " + strLongtitude);
-
-        Marker marker = mMap.addMarker(markerOptions);
-        marker.showInfoWindow();
-        markerList.add(marker);
-    }
-
-    @Override
-    public boolean onMarkerClick(@NonNull Marker marker) {
-        Toast.makeText(this,
-                marker.getTitle() + "\n" + marker.getPosition(),
-                Toast.LENGTH_LONG+300)
-                .show();
-        return false;
-    }
-
-    private void fetchDataFromApi() {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(GET_API_URL)
-                .build();
-
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    String responseData = response.body().string();
-
-                } else {
-                    // Handle error if the request was not successful
-                }
-            }
-
-            @Override
-            public void onFailure(Call call, IOException e) {
-                // Handle the failure case (e.g., network error)
-            }
-        });
-    }
-
-    private void saveDataToApi() {
-        OkHttpClient client = new OkHttpClient();
-        MediaType mediaType = MediaType.parse("application/json");
-        String yourJsonRequestBody = "{\"Lat\":\"12.34\",\"Lng\":\"1234\",\"Comment\":\"test\"}";
-        RequestBody requestBody = RequestBody.create(mediaType, yourJsonRequestBody);
-        Request request = new Request.Builder()
-                .url(POST_API_URL)
-                .post(requestBody)
-                .build();
-
+    private String convertLatLngToAddress(LatLng latLng) {
         try {
-            // Execute the API call synchronously
-            Response response = client.newCall(request).execute();
-            Toast.makeText(this, "here", Toast.LENGTH_LONG).show();
-            if (response.isSuccessful())
-            {
-                String responseData = response.body().string();
-
+            Geocoder geocoder = new Geocoder(this);
+            List<Address> addresses = null;
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            StringBuilder sb = new StringBuilder();
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                    sb.append(address.getAddressLine(i));
+                }
+                sb.deleteCharAt(sb.length() - 2);
             }
-            else
-            {
-                Toast.makeText(this, "failed", Toast.LENGTH_LONG).show();
-            }
-        } catch (IOException e) {
-            // Handle network errors here
-            e.printStackTrace();
+            return sb.toString();
         }
+        catch(Exception ex) {
+
+        }
+        return null;
     }
+    private void dropMarker(LatLng latLng, String titleValue) {
+            String address = convertLatLngToAddress(latLng);
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(latLng)
+                    .draggable(true)
+                    .title(titleValue)
+                    .snippet(address);
+
+            Marker marker = mMap.addMarker(markerOptions);
+            marker.showInfoWindow();
+            markerList.add(marker);
+    }
+    public static void showLongToast(Context context, String message, int durationInMillis) {
+        final Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+
+        toast.show();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+                public void run() {
+                    toast.cancel();
+                }
+            }, durationInMillis);
+    }
+
+    //menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Button btnStartDrawing = findViewById(R.id.btnStartDrawing);
+        Button btnStopDrawing = findViewById(R.id.btnStopDrawing);
         if (item.getItemId() == R.id.menu_marker_information) {
             showMarkerInformationDialogBox();
             return true;
-        } else if (item.getItemId() == R.id.menu_search_zip_code) {
+        } else if (item.getItemId() == R.id.menu_search) {
             showSearchDialogBox(1);
             return true;
-        } else if (item.getItemId() == R.id.menu_search_state) {
-            showSearchDialogBox(2);
+        } else if (item.getItemId() == R.id.menu_marker) {
+            mMap.clear();
+            markerList.clear();
+            polygonPoints.clear();
+            activePolygon = null;
+            polygons.clear();
+            btnStartDrawing.setVisibility(View.GONE);
+            btnStopDrawing.setVisibility(View.GONE);
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            return true;
+        } else if (item.getItemId() == R.id.menu_drawing) {
+            mMap.clear();
+            polygonPoints.clear();
+            activePolygon = null;
+            polygons.clear();
+            markerList.clear();
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            btnStartDrawing.setVisibility(View.VISIBLE);
+            btnStopDrawing.setVisibility(View.GONE);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
     private void showMarkerInformationDialogBox() {
         StringBuilder markerInfo = new StringBuilder("Marker Information:\n");
-        for (Marker marker : markerList) {
-            String title = marker.getTitle();
-            String snippet = marker.getSnippet();
-            markerInfo
-                    .append("Id: ")
-                    .append(marker.getId() + "\n")
-                    .append("Title: " + marker.getTitle() + "\n")
-                    .append("Position: :" + marker.getSnippet() + "\n\n");
-        }
+        if (markerList.isEmpty()) {markerInfo.append("none");}
+        else {
+            for (Marker marker : markerList) {
+                String title = marker.getTitle();
+                String snippet = marker.getSnippet();
+                markerInfo
+                        .append("Id: ")
+                        .append(marker.getId() + "\n")
+                        .append("Title: " + marker.getTitle() + "\n")
+                        .append("Position: :" + marker.getSnippet() + "\n\n");
+            }
+         }
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Marker Information");
@@ -244,13 +302,7 @@ public class MapsMarkerActivity extends AppCompatActivity
 
     private void showSearchDialogBox(int options) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        if (options == 1)
-        {
-            builder.setTitle("Search by Zip Code: ");
-        } else if (options == 2)
-        {
-            builder.setTitle("Search by State: ");
-        }
+        builder.setTitle("Search");
         final EditText input = new EditText(this);
         builder.setView(input);
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -270,15 +322,11 @@ public class MapsMarkerActivity extends AppCompatActivity
 
         builder.show();
     }
-
     private void moveCameraWithSearch(int options, String searchValue) {
         Geocoder geocoder = new Geocoder(this);
         try {
             List<Address> addresses = null;
-            if (options == 1)
-                addresses = geocoder.getFromLocationName(searchValue, 1);
-            else if (options == 2)
-                addresses = geocoder.getFromLocationName(searchValue, 1);
+            addresses = geocoder.getFromLocationName(searchValue, 1);
             if (addresses != null && !addresses.isEmpty()) {
                 Address address = addresses.get(0);
                 double latitude = address.getLatitude();
@@ -300,7 +348,6 @@ public class MapsMarkerActivity extends AppCompatActivity
             e.printStackTrace();
         }
     }
-
 }
 
 
